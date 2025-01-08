daf2e22deddfd65f3726372237e27229b8483ae3
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-08 10:06:37 +0100

    added first test

---
+++ Assignment/TestClass.cs
@@ -0,0 +1,9 @@
+using C_ASSIGNMENT_BUILDER.Engine.AssignmentBuilder;
+
+public class TestClass : AssignmentBase
+{
+    public string stringCheese(string cheese)
+    {
+        throw new NotImplementedException();
+    }
+}
